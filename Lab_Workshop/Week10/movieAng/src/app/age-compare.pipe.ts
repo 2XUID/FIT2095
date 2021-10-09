dcf80702a77040db321cb7a19db21721211e0ab0
AllEN <qr1403525394@outlook.com>
2021-10-09 18:01:21 +1100

    add week11 pre reading

--- Lab_Workshop/Week10/movieAng/src/app/age-compare.pipe.ts
+++ Lab_Workshop/Week10/movieAng/src/app/age-compare.pipe.ts
@@ -1,18 +1,32 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {
+  Pipe,
+  PipeTransform
+} from '@angular/core';
 
 @Pipe({
   name: 'ageCompare'
 })
 export class AgeComparePipe implements PipeTransform {
 
-  transform(value: number): string {
+  transform(value: number, args: number): string {
     let compare: string = '';
-    if(value<1980){
-      compare = 'Old'
-    }else if (value>1980&&value<2000){
-      compare = 'Middle Aged'
+    if (args == 1) {
+      if (value < 1980) {
+        compare = 'Old'
+      } else if (value > 1980 && value < 2000) {
+        compare = 'Middle Aged'
+      } else {
+        compare = 'Young'
+      }
     }else{
-      compare = 'Young'
+      let compare: string = '';
+      if (value < 2000) {
+        compare = 'Old'
+      } else if (value > 2000 && value < 2010) {
+        compare = 'Middle Aged'
+      } else {
+        compare = 'Young'
+      }
     }
     return compare
   }
